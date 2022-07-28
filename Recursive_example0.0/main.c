2eba9707cb5082e0c492db98cadbf96b6be1c857
Nevahu <nevahuarslan26@gmail.com>
2022-07-28 18:27:36 +0300

    Recursive_example0.0
    Recursive_example0.0

---
+++ Recursive_example0.0/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fun(int a[], int n)
+{
+    int x;
+    if( n==1)
+        return a[0]; /*base-case*/
+    else
+        x = fun(a, n-1);
+    if(x> a[n-1])
+        return x;
+    else
+        return a[n-1];
+}
+
+
+
+int main()
+{
+    int arr[] = {12, 10, 300, 50, 100};
+    printf(" %d ", fun(arr, 5));
+    return 0;
+}
